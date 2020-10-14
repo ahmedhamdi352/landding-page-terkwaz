a086128585a13138a4e3ec92402d2c04c1ed31d9
ahmedhamdi352 <ahmedhamdi352@gmail.com>
2020-10-14 14:06:54 +0200

    change photos

--- src/components/tabBody.js
+++ src/components/tabBody.js
@@ -1,21 +1,20 @@
+import Paragraph from 'antd/lib/skeleton/Paragraph';
 import React, { Component } from 'react'
 import pic from '../assests/images/illustration-features-tab-1.svg'
 import '../assests/styles/component/body.scss';
-const  TabBody = ()=> {
+const  TabBody = ({image, Paragraph})=> {
         return (
             <div className='body-content' style={{marginTop:'50px'}}>
 
             <div className='body-tab-continer'>
                <div className='image-feature-continer'>
-                   <img src={pic} className='image-feature'/>
+                   <img src={image} className='image-feature'/>
     
                </div>
 
                <div className='text-feature-continer' >
-                   <p className='text-bold'>  Bookmark on one click</p>
-                   <p className='text-normal'> Organize your bookmarks however you like. our simple
-                   drag-and-drop interface gives you complete control over how you manage your favourite sites
-                    </p>
+                      <p className='text-bold'> {Paragraph.title}</p>
+                   <p className='text-normal'>{Paragraph.text}</p>
                <div>
                    <button className='btn-chrome'>More info</button>
                </div>
--- src/components/taps.js
+++ src/components/taps.js
@@ -3,22 +3,41 @@ import { Tabs } from 'antd';
 import 'antd/dist/antd.css';
 import '../assests/styles/component/taps.scss';
 import TapBody from './tabBody'
+import tapShare from '../assests/images/illustration-features-tab-3.svg'
+import tapSearch from '../assests/images/illustration-features-tab-2.svg'
+import tapBookmark from '../assests/images/illustration-features-tab-1.svg'
+
 const { TabPane } = Tabs;
 
 function callback(key) {
   console.log(key);
 }
 
+const Paragraphes ={
+  tapBookmark :{
+    "title":" Bookmark on one click",
+    "text" :" Organize your bookmarks however you like. our simple drag-and-drop interface gives you complete control over how you manage your favourite sites"
+  },
+  tapSearch:{
+    "title":"Intelligent search",
+    "text":"Our powerful search feature will help you find saved sites in on time at all. No need to trawl through all of your bookmarks"
+  },
+  tapShare :{
+    "title":"Share Your bookmarks",
+    "text":"Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button"
+  }
+}
+
 const Demo = () => (
   <Tabs defaultActiveKey="1" onChange={callback} className='contnet-flex'>
     <TabPane tab="Simple Bookmarking" key="1" >
-        <TapBody/>
+        <TapBody image={tapBookmark} Paragraph ={Paragraphes.tapBookmark}/>
     </TabPane>
     <TabPane tab="Speedy Searching" key="2">
-    <TapBody/>
+    <TapBody image={tapSearch} Paragraph ={Paragraphes.tapSearch}/>
     </TabPane>
     <TabPane tab="Easy Sharing" key="3">
-    <TapBody/>
+    <TapBody image={tapShare} Paragraph ={Paragraphes.tapShare} />
     </TabPane>
   </Tabs>
 );
